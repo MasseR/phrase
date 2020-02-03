cf7491e14cac657cb1ed1cbb5872eba19c868dbe
Mats Rauhala <mats.rauhala@iki.fi>
2020-02-03 20:40:23 +0200

    Command line parsing

--- src-exe/Main.hs
+++ src-exe/Main.hs
@@ -1,4 +1,29 @@
 module Main where
 
+import           Data.Text (Text)
+
+import Options.Applicative
+
+data Args
+  = Args { argsName   :: Text
+         , argsLength :: Maybe Int
+         }
+  deriving Show
+
+parseArgs :: Parser Args
+parseArgs =
+  Args <$> parseName
+       <*> parseLength
+  where
+    parseName :: Parser Text
+    parseName = argument str (metavar "pass-name" <> help "Name of the secret")
+    parseLength :: Parser (Maybe Int)
+    parseLength = optional (option auto (metavar "pass-size" <> help "Sentence size"))
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  args <- execParser opts
+  print args
+  where
+    opts :: ParserInfo Args
+    opts = info (parseArgs <**> helper) fullDesc
