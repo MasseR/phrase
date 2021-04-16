e771b09557b5748ecfeaabb666ae2be74222180c
Mats Rauhala <mats.rauhala@iki.fi>
2021-04-16 09:11:06 +0300

    Add easy-hls

--- shell.nix
+++ shell.nix
@@ -3,17 +3,26 @@
 with nixpkgs;
 
 let
-  phrase = haskellPackages.callPackage ./. {};
+  hp = haskellPackages.extend (self: super: {
+    phrase = self.callPackage ./. {};
+  });
+  easy-hls-src = fetchFromGitHub {
+    owner = "jkachmar";
+    repo = "easy-hls-nix";
+    inherit (builtins.fromJSON (builtins.readFile ./easy-hls-nix.json)) rev sha256;
+  };
+  easy-hls = callPackage easy-hls-src { ghcVersions = [ hp.ghc.version ]; };
 
 in
 
-mkShell {
+hp.shellFor {
+  packages = h: [h.phrase];
   buildInputs = [
     entr
     haskellPackages.cabal-install
     haskellPackages.hlint
     stylish-haskell
     ghcid
-    (haskellPackages.ghcWithHoogle (_: phrase.buildInputs ++ phrase.propagatedBuildInputs))
+    easy-hls
   ];
 }
